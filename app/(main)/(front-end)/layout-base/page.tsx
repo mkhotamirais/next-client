3ea35d4c4af56536e30777e0c21047913aff0ce1
mkhotamirais <mkhotamirais@gmail.com>
2025-06-29 13:38:54 +0700

    bnyak

---
+++ app/(main)/(front-end)/layout-base/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LayoutBase() {
+  return (
+    <>
+      <header>header</header>
+      <main>halo semua</main>
+      <footer>footer</footer>
+    </>
+  );
+}
