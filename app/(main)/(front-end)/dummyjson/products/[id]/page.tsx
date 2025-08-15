a76706f5399d9be00e0a0fed83e44ec59f1222b1
mkhotamirais <mkhotamirais@gmail.com>
2025-08-15 09:32:49 +0700

    ukuran 400

--- app/(main)/(front-end)/dummyjson/products/[id]/page.tsx
+++ app/(main)/(front-end)/dummyjson/products/[id]/page.tsx
@@ -11,16 +11,24 @@ export const generateStaticParams = async () => {
 
 export default async function ProductId({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
-  const res = await dummyJsonUrl.get(`/products/${id}`);
-  const product = res.data as IProduct;
+  const res = await fetch(`https://dummyjson.com/products/${id}`, {
+    cache: "force-cache", // default-nya cache
+    next: { revalidate: 60 }, // 1 menit refresh
+  });
+  const product = (await res.json()) as IProduct;
 
   return (
     <section className="py-4">
-      <p>halo {id}</p>
       <h1 className="h1">{product.title}</h1>
       <div className="flex">
-        <Image src={product.images[0]} alt={product.title} width={400} height={400} />
-        <Image src={product.thumbnail} alt={product.title} width={400} height={400} />
+        <div>
+          <Image src={product.images[0]} alt={product.title} width={400} height={400} className="" priority />
+          <p>gambar 2</p>
+        </div>
+        <div>
+          <Image src={product.images[0]} alt={product.title} width={400} height={400} className="h-56 w-auto" />
+          <p>gambar 1</p>
+        </div>
       </div>
     </section>
   );
